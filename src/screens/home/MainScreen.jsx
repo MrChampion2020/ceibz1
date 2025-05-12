4f81b3275bd84ee40470ec5810cd981e7a4c94db
runorx <runorx.md@gmail.com>
2025-05-12 04:48:05 +0100

    merge remote changes design change

--- src/screens/home/MainScreen.jsx
+++ src/screens/home/MainScreen.jsx
@@ -192,8 +192,7 @@ const MainScreen = () => {
           transition={{ delay: 0.8, duration: 0.8 }}
           style={{
             position: "absolute",
-            bottom: isMobile ? "38%" : -110,
-            // top: isMobile ? "auto" : "auto",
+            bottom: isMobile ? 300 : -110,
             left: 0,
             right: 0,
             backgroundColor: "#f59e0b",
